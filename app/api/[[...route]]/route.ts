b97588567202a828a0ae7c043adcd4368051aa68
Jorge Oliveira <jorgeoliveira2k22@gmail.com>
2025-09-22 17:12:41 +0100

    post api

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,18 +1,9 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { HTTPException } from "hono/http-exception";
 
 import accounts from "./accounts";
 
 const app = new Hono().basePath("/api");
-
-app.onError((err, c) => {
-  if (err instanceof HTTPException) {
-    return err.getResponse();
-  }
-
-  return c.json({ error: "Internal Error" }, 500);
-});
 
 const routes = app.route("/accounts", accounts);
 
---
+++ providers/sheet-provider.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { NewAccountSheet } from "@/features/accounts/components/new-accout-sheet";
+
+export const SheetProvider = () => {
+  return (
+    <>
+      <NewAccountSheet />
+    </>
+  );
+};
