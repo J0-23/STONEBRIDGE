8c97e1b03e11b10b2605e8055a820f3db0f4cd95
Jorge Oliveira <jorgeoliveira2k22@gmail.com>
2025-09-25 09:52:26 +0100

    categories

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -3,11 +3,17 @@
 import { NewAccountSheet } from "@/features/accounts/components/new-accout-sheet";
 import { EditAccountSheet } from "@/features/accounts/components/edit-accout-sheet";
 
+import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
+import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+
 export const SheetProvider = () => {
   return (
     <>
       <NewAccountSheet />
       <EditAccountSheet />
+
+      <NewCategorySheet />
+      <EditCategorySheet />
     </>
   );
 };
